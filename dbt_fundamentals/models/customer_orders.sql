9ed851609441d74562e7cb18ee8e50f38b1461dd
Abhishek Vaish <abhishek.vaish@acsicorp.com>
2023-05-31 15:33:59 +0530

    sources

--- dbt_fundamentals/models/customer_orders.sql
+++ dbt_fundamentals/models/customer_orders.sql
@@ -1,33 +1,31 @@
-with customer as (
-    select 
-        id as customer_id,
-        first_name,
-        last_name
-    from raw.jaffle_shop.customers;
-);
+with customers as (
+
+    select * from {{ ref('stg_customers') }}
+),
 
 orders as (
-    select 
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-    from raw.jaffle_shop.orders
-);
+
+    select * from {{ ref('stg_orders') }}
+),
 
 customer_orders as (
+
     select
         customer_id,
-        
+
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
         count(order_id) as number_of_orders
+
     from orders
+
     group by 1
-);
+
+),
 
 final as (
-    select 
+
+    select
         customers.customer_id,
         customers.first_name,
         customers.last_name,
@@ -36,7 +34,9 @@ final as (
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders
 
     from customers
-    left join customer_orders using(customer_id)
+
+    left join customer_orders using (customer_id)
 
 )
-select * from final;
+
+select * from final
